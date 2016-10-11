5f97a578527dceb0e77d5c4af9333795d26c2367
runningIris <irisrunning@foxmail.com>
2016-10-11 23:54:58 +0800

    重构’登录、关注‘第一步： 把关于cookie的function移到tool.js

--- js/script.js
+++ js/script.js
@@ -11,29 +11,6 @@ function inform(){
 	} else{
 	 inform.parentNode.removeChild(inform);
 	}
-
-	function setCookie(name, value, expires){
-	 var expdate = new Date();
-	 expdate.setTime(expdate.getTime() + expires);
-	 document.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value) + ';expires=' + expires + ';path=/';
-	}
-	function getCookie(name){
-	 var cookieName = encodeURIComponent(name) + '=';
-	 var cookieStart = document.cookie.indexOf(cookieName);
-	 var cookieValue = null;
-	 if (cookieStart > -1){
-	     var cookieEnd = document.cookie.indexOf(';', cookieStart);
-	     if (cookieEnd == -1){
-	         cookieEnd = document.cookie.length;
-	     }
-	     cookieValue = decodeURIComponent(document.cookie.substring(cookieStart + cookieName.length, cookieEnd));
-	     return cookieValue;
-	 }
-	}
-	function deleteCookie(name){
-	 document.cookie = encodeURIComponent(name) + '=;expires=' + new Date(0);
-	}
-
 }
 
 
--- js/tool.js
+++ js/tool.js
@@ -13,6 +13,28 @@ function $$(node,selector){
 		return [].slice.call(arguments[0].querySelectorAll(arguments[1]));
 	}
 }
+// cookie
+function setCookie(name, value, expires){
+	var expdate = new Date();
+	expdate.setTime(expdate.getTime() + expires);
+	document.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value) + ';expires=' + expires + ';path=/';
+}
+function getCookie(name){
+	var cookieName = encodeURIComponent(name) + '=';
+	var cookieStart = document.cookie.indexOf(cookieName);
+	var cookieValue = null;
+	if (cookieStart > -1){
+    	var cookieEnd = document.cookie.indexOf(';', cookieStart);
+    	if (cookieEnd == -1){
+        	cookieEnd = document.cookie.length;
+     	}
+    	cookieValue = decodeURIComponent(document.cookie.substring(cookieStart + cookieName.length, cookieEnd));
+    	return cookieValue;
+	}
+}
+function deleteCookie(name){
+	document.cookie = encodeURIComponent(name) + '=;expires=' + new Date(0);
+}
 
 //把字符串转化为节点
 function html2node(str){
